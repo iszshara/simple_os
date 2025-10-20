6eaebc1f74107461ed29e7783a6e3f3351923597
Iszshara <luis.rennert44@gmail.com>
2025-10-20 16:56:47 +0200

    added: Test for a Stack Overflow

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -1,3 +1,5 @@
+//! Testet ob ein Stack Overflow passieren kann
+
 #![no_std]
 #![no_main]
 #![feature(abi_x86_interrupt)]
@@ -7,6 +9,13 @@ use simple_os::{serial_print, serial_println, exit_qemu, QemuExitCode};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
+////////////////////////////////////////////////////////////////////////////////
+///
+/// ## Eingangspunkt
+/// 
+/// Startet die Funktion die einen Stack Overflow herbeiführt.
+/// 
+////////////////////////////////////////////////////////////////////////////////
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !
 {
@@ -20,6 +29,14 @@ pub extern "C" fn _start() -> !
     panic!("Execution continued after stack overflow");
 }
 
+////////////////////////////////////////////////////////////////////////////////
+///
+/// ## test_double_fault_handler()
+/// 
+/// Implementiert einen Double Fault Handler zum Testen, bei dem ein Stack Frame
+/// und ein Error Code mitgegeben wird.
+/// 
+////////////////////////////////////////////////////////////////////////////////
 extern "x86-interrupt" fn test_double_fault_handler(
     _stack_frame: InterruptStackFrame,
     _error_code: u64,
@@ -30,6 +47,23 @@ extern "x86-interrupt" fn test_double_fault_handler(
     loop{}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+///
+/// ## stack_overflow()
+/// 
+/// Funktion die einen Stack Overflow herbeiführt.
+/// Die Flag '#[allow(unconditional_recursion)]' muss genutzt werden da der 
+/// Kompiler (richtigerweise) das Programm nicht kompiliert.
+/// 
+/// ### Erklärung 
+/// 
+/// stack_overflow() -> Die Adresse der nächsten Anweisung nach dem 
+/// Funktionsaufruf (die Rücksprungadresse) wird auf den Stack gelegt -> die 
+/// Funktion wird erneut ausgeführt. Da die Funktion keine Abbruchbedingung hat
+/// ruft sie sich immer wieder selbst auf und legt jedes mal eine weitere
+/// Rücksprungadresse auf den Stack, der somit vollläuft.
+/// 
+////////////////////////////////////////////////////////////////////////////////
 #[allow(unconditional_recursion)]
 fn stack_overflow()
 {
@@ -58,6 +92,13 @@ lazy_static!
     };
 }
 
+////////////////////////////////////////////////////////////////////////////////
+///
+/// ## init_test_idt()
+/// 
+/// Lädt eine Interrupt Descriptor Table zum Testen.
+/// 
+////////////////////////////////////////////////////////////////////////////////
 pub fn init_test_idt()
 {
     TEST_IDT.load();
